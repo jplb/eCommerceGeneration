fbb61a5f88eccb573f3dc89040cc96bf01ae3311
Luana Bezana <luana.bezana@hotmail.com>
2021-06-05 17:58:37 -0300

    Commit inicial

---
+++ src/test/java/com/eCommerceGen/helloWorldTShirts/HelloWorldTShirtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerceGen.helloWorldTShirts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldTShirtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
