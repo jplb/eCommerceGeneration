f0e5879075418799d68638d994dcb8018c1047fb
Luana Bezana <luana.bezana@hotmail.com>
2021-06-05 23:31:22 -0300

    Subindo commit inicial do projeto

---
+++ BackEnd/LojaPrincipal/src/test/java/com/IntegradorG3/LojaPrincipal/LojaPrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntegradorG3.LojaPrincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaPrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
