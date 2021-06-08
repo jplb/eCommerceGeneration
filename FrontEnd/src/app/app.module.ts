7355701241a90cc7d7390c771be87e8a129fa545
Maicon <hrqn.fernandes@gmail.com>
2021-06-08 10:35:07 -0300

    Adicionadas componentes de autenticacao (login) e cadastro de novos usuarios. Adicionado consumo de API

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd/src/app/model/User.ts
@@ -0,0 +1,10 @@
+export class User {
+
+    public id: number
+    public nomeCompleto:  string
+    public email: string
+    public senha: string
+    public telefoneFixo: string
+    public telefoneCelular: string
+    public endereco: string
+}
