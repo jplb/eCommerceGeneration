8203ddcda18dbcf5138da27cb4d37d4b4095a77b
Maicon <hrqn.fernandes@gmail.com>
2021-06-11 02:17:21 -0300

    commit required to pull

--- FrontEnd/src/app/model/User.ts
+++ FrontEnd/src/app/model/User.ts
@@ -8,3 +8,4 @@ export class User {
     public telefoneCelular: string
     public endereco: string
 }
+
---
+++ FrontEnd/src/app/produto/produto.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Produto } from '../model/Produto';
+import { ProdutoService } from '../service/produto.service';
+
+@Component({
+  selector: 'app-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css']
+})
+export class ProdutoComponent implements OnInit {
+
+  produto: Produto = new Produto() 
+  listaProdutos: Produto[]
+
+  constructor(
+
+
+    private router: Router,
+    private produtoService: ProdutoService
+  ) { }
+
+  ngOnInit() {
+
+
+     
+     this.findAllProdutos()
+ }
+
+ findAllProdutos(){
+  this.produtoService.getAllProdutos().subscribe((resp: Produto[])=>{
+    this.listaProdutos = resp
+  })
+ }
+
+ cadastrar(){
+  this.produtoService.postProduto(this.produto).subscribe((resp: Produto)=>{
+    this.produto = resp
+    alert('Produto cadastrado com sucesso!')
+    this.findAllProdutos()
+    this.produto = new Produto() 
+  })
+
+ }
+
+  }
+
+
