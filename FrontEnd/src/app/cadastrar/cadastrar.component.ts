94ca0ee6f1bdb539f28b30507835edd8b101ce1b
Maicon <hrqn.fernandes@gmail.com>
2021-06-16 17:21:25 -0300

    Implementado validação de entrada do cadastrar

--- FrontEnd/src/app/cadastrar/cadastrar.component.ts
+++ FrontEnd/src/app/cadastrar/cadastrar.component.ts
@@ -13,6 +13,10 @@ export class CadastrarComponent implements OnInit {
 
   user: User = new User()
   confirmarSenha: string
+  nomeValido: boolean = false;
+  emailValido: boolean = false;
+  senhaValida: boolean = false;
+  senhasIguais: boolean = false;
 
   constructor(
     private authService: AuthService,
@@ -28,6 +32,8 @@ confirmSenha(event:any){
   this.confirmarSenha = event.target.value
 }
 
+
+
 cadastrar(){
   if(this.user.senha != this.confirmarSenha){
     alert('Senhas Diferentes!')
@@ -39,5 +45,45 @@ cadastrar(){
     })
   }
 }
+
+
+/* Validação de entrada  */
+
+validacao(condicao: boolean, event:any){
+  let valid = false;
+  if(condicao){
+    event.target.classList.remove("is-valid");
+    event.target.classList.add("is-invalid");
+  }else{
+    event.target.classList.remove("is-invalid");
+    event.target.classList.add("is-valid");
+    valid = true;
+  }
+  return valid;
+}
+
+validaNome(event: any){
+  this.nomeValido = this.validacao(event.target.value.length < 3, event);
+  
+  
+}
+
+validaEmail(event: any){
+  this.emailValido = this.validacao(event.target.value.indexOf('@') == -1 || event.target.value.indexOf('.') == -1, event)
+  
+}
+
+validaSenha(event: any){
+  this.senhaValida = this.validacao(event.target.value.length < 6 || event.target.value.length > 20, event)
+ 
+}
+
+confirmaSenhas(event: any){
+  this.senhasIguais = this.validacao(this.user.senha != this.confirmarSenha, event)
+ 
+}
+
+
+
 
 }
